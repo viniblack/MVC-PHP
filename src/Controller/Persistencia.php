fba4f7a1414443f88c73e45fc3cdb3b1ebacc70c
Vini Black <viniibl4ck@gmail.com>
2021-12-14 14:20:55 -0300

    criando filtro de dados

--- src/Controller/Persistencia.php
+++ src/Controller/Persistencia.php
@@ -22,8 +22,13 @@ class Persistencia implements InterfaceControladorRequisicao
 
   public function processaRequisicao(): void
   {
+    $descricao = filter_input(
+      INPUT_POST,
+      'descricao',
+      FILTER_SANITIZE_STRING
+    );
     $curso = new Curso();
-    $curso->setDescricao($_POST['descricao']);
+    $curso->setDescricao($descricao);
     $this->entityManager->persist($curso);
     $this->entityManager->flush();
   }
