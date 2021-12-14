dd75b60d695647b2256c4572661a8e230903d326
Vini Black <viniibl4ck@gmail.com>
2021-12-14 14:40:05 -0300

    criando rotas para as paginas

--- public/index.php
+++ public/index.php
@@ -2,25 +2,17 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Alura\Cursos\Controller\FormularioInsercao;
-use Alura\Cursos\Controller\ListarCursos;
-use Alura\Cursos\Controller\Persistencia;
+use Alura\Cursos\Controller\InterfaceControladorRequisicao;
 
-switch ($_SERVER['PATH_INFO']) {
-  case '/listar-cursos':
-    $controlador = new ListarCursos();
-    $controlador->processaRequisicao();
-    break;
-  case '/novo-curso':
-    $controlador = new FormularioInsercao();
-    $controlador->processaRequisicao();
-    break;
+$caminho = $_SERVER['PATH_INFO'];
+$rotas = require __DIR__ . '/../config/routes.php';
 
-  case '/salvar-curso':
-    $controlador = new Persistencia();
-    $controlador->processaRequisicao();
-    break;
-  default:
-    echo "Erro 404";
-    break;
+if (!array_key_exists($caminho, $rotas)) {
+  http_response_code(404);
+  exit();
 }
+
+$classeControladora = $rotas[$caminho];
+/** @var InterfaceControladorRequisicao $controlador */
+$controlador = new $classeControladora();
+$controlador->processaRequisicao();
