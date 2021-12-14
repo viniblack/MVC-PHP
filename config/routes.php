59ad9359cf381e9060748069dc59d8069c28f2c6
Vini Black <viniibl4ck@gmail.com>
2021-12-14 18:27:51 -0300

    criando sistema de login

--- config/routes.php
+++ config/routes.php
@@ -1,18 +1,20 @@
 <?php
 
-use Alura\Cursos\Controller\{
-  Exclusao,
-  FormularioEdicao,
-  FormularioInsercao,
-  ListarCursos,
-  Persistencia
-};
+use Alura\Cursos\Controller\{Exclusao,
+    FormularioEdicao,
+    FormularioInsercao,
+    FormularioLogin,
+    ListarCursos,
+    Persistencia,
+    RealizarLogin};
 
 return [
-  '/listar-cursos' => ListarCursos::class,
-  '/novo-curso' => FormularioInsercao::class,
-  '/salvar-curso' => Persistencia::class,
-  '/excluir-curso' => Exclusao::class,
-  '/alterar-curso' => FormularioEdicao::class
-
+    '/listar-cursos' => ListarCursos::class,
+    '/novo-curso' => FormularioInsercao::class,
+    '/salvar-curso' => Persistencia::class,
+    '/excluir-curso' => Exclusao::class,
+    '/alterar-curso' => FormularioEdicao::class,
+    '/login' => FormularioLogin::class,
+    '/realiza-login' => RealizarLogin::class,
 ];
+
